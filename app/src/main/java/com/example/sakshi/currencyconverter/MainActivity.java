3e81498bf4165fed1b310bb35dfe64b1b374af21
sakshi013 <sakshibhat013@gmail.com>
2018-08-28 21:38:10 +0530

    first commit

---
+++ app/src/main/java/com/example/sakshi/currencyconverter/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.sakshi.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void CurrencyConverter(
+        View view){
+
+        Log.i("cc", "currencyConvertor: ");
+
+        EditText editText = (EditText) findViewById(R.id.editText2);
+        String amountInPound = editText.getText().toString();
+        double amountPoundDouble = Double.parseDouble(amountInPound);
+        double amountInDollar = amountPoundDouble * 1.3;
+        String amountDollarString = String.format("%.2f",amountInDollar);
+
+        Toast.makeText(this,amountDollarString + " $",Toast.LENGTH_SHORT).show();
+
+//        Log.i("info","Button pressed");
+//        EditText editText =(EditText) findViewById(R.id.editText2);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
